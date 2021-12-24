0610f68b0c0a34154e1fa69ee2ed91db006913cf
dkochergin <dkochergin@luxoft.com>
2021-12-24 17:01:26 +0200

    Image carousel for fruits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Carousel} from "react-responsive-carousel";
+
+const descriptions = [
+    ["banana", "БАНАН"],
+    ["strawberry", "КЛУБНИКА"],
+    ["kiwi", "КИВИ"],
+    ["peach", "ПЕРСИК"],
+    ["orange", "АПЕЛЬСИН"],
+    ["pear", "ГРУША"],
+    ["lemon", "ЛИМОН"],
+    ["plum", "СЛИВА"],
+    ["apple", "ЯБЛОКО"],
+    ["pineapple", "АНАНАС"],
+    ["cherry", "ВИШНЯ"],
+    ["pomegranate", "ГРАНАТ"],
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            123
+            <Carousel showArrows={true} swipeable={true} autoPlay={true} infiniteLoop={true} emulateTouch={true}>
+                {descriptions.map((description) => {
+                    return <div>
+                        <img src={process.env.PUBLIC_URL + "/images/" + description[0] + ".jpg"}/>
+                        <p className="legend">{description[1]}</p>
+                    </div>
+                })}
+
+            </Carousel>
+        </div>
+    );
 }
 
 export default App;
